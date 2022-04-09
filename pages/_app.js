24b16919602c798cab8a0ccce730e5cc726895dd
jot2ka <j2k.koszewski@gmail.com>
2022-04-09 20:38:30 +0200

    add title to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { Fragment } from 'react'
+import Head from 'next/head'
 import '../styles/globals.scss'
 
 import Background from '../components/background/Background'
@@ -6,6 +7,9 @@ import Background from '../components/background/Background'
 export default function App({ Component, pageProps }) {
 	return (
 		<Fragment>
+			<Head>
+				<title>Discord auth form</title>
+			</Head>
 			<Background />
 			<Component {...pageProps} />
 		</Fragment>
